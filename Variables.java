5d21a4f1e7c57c2b41a8463863f073e61030acfa
garethlandy <35927941+garethlandy@users.noreply.github.com>
2019-04-08 22:47:10 +0100

    Variable naming standard
    Single use or temporary variable names

--- Variables.java
+++ Variables.java
@@ -2,13 +2,22 @@
 public class Variables {
 	
 	//Variable naming standard
-	private String name;
-	private int age;
+	private String _name; // Do not start variables with an underscore ('_')
+	private int $age;	//or a dollar sign
 	
-	//Single character variables
-	private int i;
+	//Single character variables	
+	//Should only be used for temporary variables name	
+	public void myMethod() {
+	for(int i = 0 ; i < 5 ; i ++) {
+		System.out.println(i);
+		// i is only used within the scope of the loop and is not called 
+		// by any other function within the class		
+	}
+	}
 	
-	//Constants
+	
+	
+	//Constant variable name should always be in UPPERCASE
 	private static final String ROOT_URL = "https://123.321.23.2/mysql/users.php";
 	
 	//Methods
