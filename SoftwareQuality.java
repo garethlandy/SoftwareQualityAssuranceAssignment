654b47017b3e694a289c9ceaa85559c3142b48e6
garethlandy <35927941+garethlandy@users.noreply.github.com>
2019-04-09 00:46:32 +0100

    Structure of Source Files

---
+++ SoftwareQuality.java
@@ -0,0 +1,15 @@
+package com.tudublin.qualitysoftware;
+
+/**
+ * @author Gareth Landy
+ * mailto:c15735901@mydit.ie
+ *
+ */
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class SoftwareQuality {
+	
+	
+}
