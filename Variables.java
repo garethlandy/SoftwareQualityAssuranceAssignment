d0a4cee366298f62aa8a7ca595c68b4114925cc7
garethlandy <35927941+garethlandy@users.noreply.github.com>
2019-04-08 22:39:07 +0100

    Variables

---
+++ Variables.java
@@ -0,0 +1,32 @@
+
+public class Variables {
+	
+	//Variable naming standard
+	private String name;
+	private int age;
+	
+	//Single character variables
+	private int i;
+	
+	//Constants
+	private static final String ROOT_URL = "https://123.321.23.2/mysql/users.php";
+	
+	//Methods
+	//A verb is typically used here. 
+	public String getName() { 
+		return name;
+	}
+	
+	//Each named variable starts with a lower case followed by an uppercase letter on each new word
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	                 
+	
+	
+	
+}
+
